4d4618983ae2ee44bd10dd1fd464250cb3be8526
Pawan Kumar <ratwanapawan@gmail.com>
2024-01-20 13:01:51 +0530

    basic setup of ejs router and controller

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+// set the routes 
+app.use('/', require('./routes'));
+
 app.get('/', (req, res) => {
     res.send("Jai Shree Ram")
 })
+
+
+// configure view engine and view directory 
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, (err) => {
     if (err) {
